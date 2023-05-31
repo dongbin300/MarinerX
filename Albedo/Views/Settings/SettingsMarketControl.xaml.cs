fe60ed8e9b26d377d0cbe6e4b157150dc01eaeff
Gaten <dongbin300@gmail.com>
2023-05-31 10:21:42 +0900

    Market secret key links to PasswordBox

--- Albedo/Views/Settings/SettingsMarketControl.xaml.cs
+++ Albedo/Views/Settings/SettingsMarketControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Albedo.Views.Settings
 {
@@ -7,16 +8,95 @@ namespace Albedo.Views.Settings
     /// </summary>
     public partial class SettingsMarketControl : UserControl
     {
+        bool isChanging = false;
+
         public SettingsMarketControl()
         {
             InitializeComponent();
 
+            BinanceApiSecretKeyPassword.Visibility = Visibility.Visible;
+            UpbitApiSecretKeyPassword.Visibility = Visibility.Visible;
+            BithumbApiSecretKeyPassword.Visibility = Visibility.Visible;
+            BinanceApiSecretKeyText.Visibility = Visibility.Collapsed;
+            UpbitApiSecretKeyText.Visibility = Visibility.Collapsed;
+            BithumbApiSecretKeyText.Visibility = Visibility.Collapsed;
+
             BinanceApiKeyText.Text = Albedo.Settings.Default.BinanceApiKey;
-            BinanceApiSecretKeyText.Text = Albedo.Settings.Default.BinanceSecretKey;
+            BinanceApiSecretKeyText.Text = BinanceApiSecretKeyPassword.Password = Albedo.Settings.Default.BinanceSecretKey;
             UpbitApiKeyText.Text = Albedo.Settings.Default.UpbitApiKey;
-            UpbitApiSecretKeyText.Text = Albedo.Settings.Default.UpbitSecretKey;
+            UpbitApiSecretKeyText.Text = UpbitApiSecretKeyPassword.Password = Albedo.Settings.Default.UpbitSecretKey;
             BithumbApiKeyText.Text = Albedo.Settings.Default.BithumbApiKey;
-            BithumbApiSecretKeyText.Text = Albedo.Settings.Default.BithumbSecretKey;
+            BithumbApiSecretKeyText.Text = BithumbApiSecretKeyPassword.Password = Albedo.Settings.Default.BithumbSecretKey;
+        }
+
+        private void BinanceEyeImage_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (BinanceApiSecretKeyPassword.Visibility == Visibility.Visible)
+            {
+                BinanceApiSecretKeyPassword.Visibility = Visibility.Collapsed;
+                BinanceApiSecretKeyText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BinanceApiSecretKeyPassword.Visibility = Visibility.Visible;
+                BinanceApiSecretKeyText.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void UpbitEyeImage_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (UpbitApiSecretKeyPassword.Visibility == Visibility.Visible)
+            {
+                UpbitApiSecretKeyPassword.Visibility = Visibility.Collapsed;
+                UpbitApiSecretKeyText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                UpbitApiSecretKeyPassword.Visibility = Visibility.Visible;
+                UpbitApiSecretKeyText.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void BithumbEyeImage_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (BithumbApiSecretKeyPassword.Visibility == Visibility.Visible)
+            {
+                BithumbApiSecretKeyPassword.Visibility = Visibility.Collapsed;
+                BithumbApiSecretKeyText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BithumbApiSecretKeyPassword.Visibility = Visibility.Visible;
+                BithumbApiSecretKeyText.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void SecretKey_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+            {
+                return;
+            }
+
+            isChanging = true;
+            BinanceApiSecretKeyPassword.Password = BinanceApiSecretKeyText.Text;
+            UpbitApiSecretKeyPassword.Password = UpbitApiSecretKeyText.Text;
+            BithumbApiSecretKeyPassword.Password = BithumbApiSecretKeyText.Text;
+            isChanging = false;
+        }
+
+        private void SecretKey_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (isChanging)
+            {
+                return;
+            }
+
+            isChanging = true;
+            BinanceApiSecretKeyText.Text = BinanceApiSecretKeyPassword.Password;
+            UpbitApiSecretKeyText.Text = UpbitApiSecretKeyPassword.Password;
+            BithumbApiSecretKeyText.Text = BithumbApiSecretKeyPassword.Password;
+            isChanging = false;
         }
     }
 }
